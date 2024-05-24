67651eabba8b896133d6728ac0a81af60deeb230
clsty <clsty@celestialy.top>
2024-05-24 23:47:41 +0800

    Sync upstream

--- .config/ags/modules/cheatsheet/data_keybinds.js
+++ .config/ags/modules/cheatsheet/data_keybinds.js
@@ -1,122 +1,152 @@
+// vim:fileencoding=utf-8:ft=javascript:fdm=marker foldlevel=0
+// In vim/neovim, press `z' `a' to toggle folding.
+
+// https://github.com/end-4/dots-hyprland/issues/167
+// The icons' names are Labels that have the font family set to "Material Symbols Rounded", see the cheatsheet here: https://fonts.google.com/icons
 export const keybindList = [[
+//{{{ ========== Windows ==========
     {
-        "icon": "pin_drop",
-        "name": "Workspaces: navigation",
+        "icon": "move_group",
+        "name": "windows",
         "binds": [
-            { "keys": ["󰖳", "+", "#"], "action": "Go to workspace #" },
-            { "keys": ["󰖳", "+", "S"], "action": "Toggle special workspace" },
-            { "keys": ["󰖳", "+", "(Scroll ↑↓)"], "action": "Go to workspace -1/+1" },
-            { "keys": ["Ctrl", "󰖳", "+", "←"], "action": "Go to workspace on the left" },
-            { "keys": ["Ctrl", "󰖳", "+", "→"], "action": "Go to workspace on the right" },
-            { "keys": ["󰖳", "+", "PageUp"], "action": "Go to workspace on the left" },
-            { "keys": ["󰖳", "+", "PageDown"], "action": "Go to workspace on the right" }
+            // Move focus #####
+            { "keys": ["s-", "hjkl"], "action": "go to" },
+            // Swap windows #####
+            { "keys": ["s-S-", "hjkl"], "action": "Swap to" },
+            // Window split ratio #####
+            { "keys": ["s-A-", "hjkl"], "action": "Resize" },
+            // Misc #####
+            { "keys": ["s-", "X"], "action": "Kill" },
+            { "keys": ["s-S-", "X"], "action": "hyprctl Kill" },
+            { "keys": ["s-A-", "Space"], "action": "floating" },
+            { "keys": ["s-", ";"], "action": "switch split" },
+            { "keys": ["s-", "F"], "action": "Fullscr" },
+            { "keys": ["s-A-", "F"], "action": "fake Fullscr" },
+            { "keys": ["s-", "Z"], "action": "move by cursor" },
+            { "keys": ["s-", "Lmb(Drag)"], "action": "Move" },
+            { "keys": ["s-", "Mmb(Drag)"], "action": "Move" },
+            { "keys": ["s-", "Rmb(Drag)"], "action": "Resize" },
         ],
         "id": 1
     },
+//}}}EndOf ===== Windows
+//{{{ ========== Workspaces ==========
     {
-        "icon": "overview_key",
-        "name": "Workspaces: management",
+        "icon": "pin_drop",
+        "name": "WorkSpace: go to",
         "binds": [
-            { "keys": ["󰖳", "Alt", "+", "#"], "action": "Move window to workspace #" },
-            { "keys": ["󰖳", "Alt", "+", "S"], "action": "Move window to special workspace" },
-            { "keys": ["󰖳", "Alt", "+", "PageUp"], "action": "Move window to workspace on the left" },
-            { "keys": ["󰖳", "Alt", "+", "PageDown"], "action": "Move window to workspace on the right" }
+            // goto #####
+            { "keys": ["s-", "num #"], "action": "WS #" },
+            { "keys": ["s-", "`"], "action": "Special WS" },
+            { "keys": ["s-", "Scroll"], "action": "WS -1/+1" },
+            { "keys": ["s-", "[ ]"], "action": "WS -1/+1" },
         ],
         "id": 2
     },
     {
-        "icon": "move_group",
-        "name": "Windows",
+        "icon": "overview_key",
+        "name": "WorkSpace: Swap to",
         "binds": [
-            { "keys": ["󰖳", "+", "←↑→↓"], "action": "Focus window in direction" },
-            { "keys": ["󰖳", "Shift", "+", "←↑→↓"], "action": "Swap window in direction" },
-            { "keys": ["󰖳", "+", ";"], "action": "Split ratio -" },
-            { "keys": ["󰖳", "+", "'"], "action": "Split ratio +" },
-            { "keys": ["󰖳", "+", "Lmb"], "action": "Move window" },
-            { "keys": ["󰖳", "+", "Rmb"], "action": "Resize window" },
-            { "keys": ["󰖳", "Alt", "+", "Space"], "action": "Float window" },
-            { "keys": ["󰖳", "+", "F"], "action": "Fullscreen" },
-            { "keys": ["󰖳", "Alt", "+", "F"], "action": "Fake fullscreen" }
+            // Move #####
+            { "keys": ["s-S-", "num #"], "action": "WS #" },
+            { "keys": ["s-S-", "`"], "action": "Special WS" },
+            { "keys": ["s-S-", "Scroll"], "action": "WS -1/+1" },
+            { "keys": ["s-S-", "[ ]"], "action": "WS -1/+1" },
         ],
         "id": 3
-    }
+    },
+//}}}EndOf ===== Workspaces
 ],
 [
+//{{{ ========== Settings & Configs ==========
     {
-        "icon": "widgets",
-        "name": "Widgets (AGS)",
+        "icon": "construction",
+        "name": "settings & configs",
         "binds": [
-            { "keys": ["󰖳", "OR", "󰖳", "+", "Tab"], "action": "Toggle overview/launcher" },
-            { "keys": ["Ctrl", "󰖳", "+", "R"], "action": "Restart AGS" },
-            { "keys": ["󰖳", "+", "/"], "action": "Toggle this cheatsheet" },
-            { "keys": ["󰖳", "+", "N"], "action": "Toggle system sidebar" },
-            { "keys": ["󰖳", "+", "B", "OR", "󰖳", "+", "O"], "action": "Toggle utilities sidebar" },
-            { "keys": ["󰖳", "+", "K"], "action": "Toggle virtual keyboard" },
-            { "keys": ["Ctrl", "Alt", "+", "Del"], "action": "Power/Session menu" },
-
-            { "keys": ["Esc"], "action": "Exit a window" },
-            { "keys": ["rightCtrl"], "action": "Dismiss/close sidebar" },
-
-            { "keys": ["Ctrl", "󰖳", "+", "T"], "action": "Change wallpaper+colorscheme" },
-
-            // { "keys": ["󰖳", "+", "B"], "action": "Toggle left sidebar" },
-            // { "keys": ["󰖳", "+", "N"], "action": "Toggle right sidebar" },
-            // { "keys": ["󰖳", "+", "G"], "action": "Toggle volume mixer" },
-            // { "keys": ["󰖳", "+", "M"], "action": "Toggle useless audio visualizer" },
-            // { "keys": ["(right)Ctrl"], "action": "Dismiss notification & close menus" }
+            { "keys": ["s-C-", "I"], "action": "Gnome settings" },
+            { "keys": ["s-C-", "V"], "action": "pavucontrol" },
+            { "keys": ["S-C-", "ESC"], "action": "sys-monitor" },
+            { "keys": ["A-S-C-", "ESC"], "action": "btop" },
+            { "keys": ["A-S-C-", "ESC"], "action": "btop" },
         ],
         "id": 4
     },
-    {
-        "icon": "construction",
-        "name": "Utilities",
-        "binds": [
-            { "keys": ["PrtSc"], "action": "Screenshot  >>  clipboard" },
-            { "keys": ["Ctrl", "PrtSc"], "action": "Screenshot  >>  file + clipboard" },
-            { "keys": ["󰖳", "Shift", "+", "S"], "action": "Screen snip  >>  clipboard" },
-            { "keys": ["󰖳", "Shift", "+", "T"], "action": "Image to text  >>  clipboard" },
-            { "keys": ["󰖳", "Shift", "+", "C"], "action": "Color picker" },
-            { "keys": ["󰖳", "Alt", "+", "R"], "action": "Record region" },
-            { "keys": ["Ctrl", "Alt", "+", "R"], "action": "Record region with sound" },
-            { "keys": ["󰖳", "Shift", "Alt", "+", "R"], "action": "Record screen with sound" }
-        ],
-        "id": 5
-    },
-],
-[
+//}}}EndOf ===== Settings & Configs
+//{{{ ========== Applications ==========
     {
         "icon": "apps",
         "name": "Apps",
         "binds": [
-            { "keys": ["󰖳", "+", "T"], "action": "Launch terminal: foot" },
-            { "keys": ["󰖳", "+", "W"], "action": "Launch browser: Firefox" },
-            { "keys": ["󰖳", "+", "C"], "action": "Launch editor: vscode" },
-            { "keys": ["󰖳", "+", "X"], "action": "Launch editor: GNOME Text Editor" },
-            { "keys": ["󰖳", "+", "I"], "action": "Launch settings: GNOME Control center" }
+            // App launcher #####
+            { "keys": ["s-", "/"], "action": "App Launcher" },
+            // Misc #####
+            { "keys": ["s-", "Return"], "action": "Tilix" },
+            { "keys": ["s-S-", "Return"], "action": "Foot" },
+        ],
+        "id": 5
+    },
+//}}}EndOf ===== Applications
+//{{{ ========== Widgets ==========
+    {
+        "icon": "widgets",
+        "name": "widgets",
+        "binds": [
+            // Power & Lockscreen #####
+            { "keys": ["s-", "del"], "action": "wlogout" },
+            { "keys": ["A-C-", "del"], "action": "ags \"session\"" },
+            { "keys": ["s-", "F1"], "action": "ScreenLock" },
+            { "keys": ["s-A-S-C-", "del"], "action": "poweroff" },
+            // MISC #####
+            { "keys": ["s-C-", "R"], "action": "restart ags" },
+            { "keys": ["s-A-C-", "R"], "action": "reload widgets" },
+            { "keys": ["s-", "."], "action": "emoji" },
+            { "keys": ["s-C-", "T"], "action": "switch wallpaper" },
+            { "keys": ["s-C-", "[ ]"], "action": "L/R sidebar" },
+            { "keys": ["s-", ","], "action": "Color Scheme" },
+            // { "keys": ["(right)Ctrl"], "action": "Dismiss notification & close menus" }
         ],
         "id": 6
     },
+//}}}EndOf ===== Widgets
+],
+[
+//{{{ ========== Capture ==========
     {
-        "icon": "keyboard",
-        "name": "Typing",
+        "icon": "center_focus_strong",
+        "name": "Capture",
         "binds": [
-            { "keys": ["󰖳", "+", "V"], "action": "Clipboard history  >>  clipboard" },
-            { "keys": ["󰖳", "+", "."], "action": "Emoji picker  >>  clipboard" },
+            // Screenshot #####
+            { "keys": ["Print"], "action": "shot screen to clipboard" },
+            { "keys": ["s-S-", "S"], "action": "shot to clipboard" },
+            { "keys": ["s-A-S-", "S"], "action": "shot and edit" },
+            // OCR #####
+            { "keys": ["s-S-C-", "S"], "action": "OCR to clipboard" },
+            // Color picker, Clipboard history #####
+            { "keys": ["s-S-", "C"], "action": "color pick" },
+            { "keys": ["s-", "V"], "action": "clipboard history" },
+            // Recorder #####
+            { "keys": ["s-", "G"], "action": "record selected" },
+            { "keys": ["s-S-", "G"], "action": "record fullscreen" },
         ],
         "id": 7
     },
+//}}}EndOf ===== Capture
+//{{{ ========== Media related ==========
     {
-        "icon": "terminal",
-        "name": "Launcher actions",
+        "icon": "stock_media",
+        "name": "Media related",
         "binds": [
-            { "keys": [">raw"], "action": "Toggle mouse acceleration" },
-            { "keys": [">img"], "action": "Select wallpaper and generate colorscheme" },
-            { "keys": [">light"], "action": "Switch to light theme" },
-            { "keys": [">dark"], "action": "Switch to dark theme" },
-            { "keys": [">badapple"], "action": "Apply black n' white colorscheme" },
-            { "keys": [">color"], "action": "Pick acccent color" },
-            { "keys": [">todo"], "action": "Type something after that to add a To-do item" },
+            // Volume #####
+            { "keys": ["(Volume)"], "action": "alter volume" },
+            // Brightness #####
+            { "keys": ["(Brightness)"], "action": "alter brightness" },
+            // Media player #####
+            { "keys": ["(Player)"], "action": "alter player" },
+            { "keys": ["s-S-", "N"], "action": "next" },
+            { "keys": ["s-S-", "B"], "action": "previous" },
+            { "keys": ["s-", "M"], "action": "Music panel" },
         ],
         "id": 8
     }
+//}}}EndOf ===== Media related
 ]];
